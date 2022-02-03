1d7b70c0242717fe0a5172da82fd521449c10ac1
den <alexandrovich07@list.ru>
2022-02-03 17:09:11 +0300

    add models, views

--- chat_prj/chat/urls.py
+++ chat_prj/chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    # path('', RoomsList.as_view()),
+    path('', MessagesList.as_view()),
 ]
--- chat_prj/chat/views.py
+++ chat_prj/chat/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
-def index(request):
-    print("hello")
-    return render(request, '1.html')
+
+class RoomsList(ListView):
+    models = Room
+    template_name = 'chat.html'
+    context_object_name = 'rooms'
+    queryset = Room.objects.all()
+
+class MessagesList(ListView):
+    models = Messages
+    template_name = 'chat.html'
+    context_object_name = 'messages'
+    queryset = Messages.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rooms'] = Room.objects.all()
+        return context
